38d17fac62f0eab1b66b8201f4f22a6db5bc6a09
David Ortega Garcia <daorga77@gmail.com>
2025-09-18 06:39:03 +0200

    Basic structure of epoll done (Son las 7 de la mañana, me quiero matar)

--- include/utilsCC.hpp
+++ include/utilsCC.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "../include/ServerConfig.hpp"
-
+#include "../include/initServer.hpp"
 
 std::string to_stringCC(int num);
-void closeListenSockets(std::vector<int> &listenSockets);
+void closeListenSockets(std::vector<t_socket> &listenSockets);
--- src/main2.cpp
+++ src/main2.cpp
@@ -17,7 +17,8 @@ int main() {
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << std::endl;
+		return(EXIT_FAILURE);
 	}
-	
+	return(EXIT_SUCCESS);
 
 }
