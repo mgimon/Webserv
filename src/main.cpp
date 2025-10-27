96d08024d690acd1d5cb33513c208da08f15a222
alphbarry <alphaomar9@gmail.com>
2025-10-27 19:40:57 +0100

    permissions

--- src/main.cpp
+++ src/main.cpp
@@ -21,23 +21,3 @@ int main(int argc, char* argv[]) {
     }
     return EXIT_SUCCESS;
 }
-/*
-int main() {
-    std::vector<ServerConfig> serverList;
-    ServerConfig server;
-    ServerConfig server2;
-    //server.addListen((t_listen){ "127.0.0.1", 8080, 128});
-    utils::hardcodeMultipleLocServer(server);
-    serverList.push_back(server);
-    server.print();
-    try
-    {
-        initServer(serverList);
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-        return(EXIT_FAILURE);
-    }
-    return(EXIT_SUCCESS);
-}*/
